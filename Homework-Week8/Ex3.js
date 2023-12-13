68b48b9b1ce78351ff6ae1c10c4cabf2356b4d01
Dinh-Linh <140419823+Dinh-Linh@users.noreply.github.com>
2023-12-13 23:11:48 +0700

    learning

--- Homework-Week8/Ex3.js
+++ Homework-Week8/Ex3.js
@@ -15,3 +15,14 @@ function countLastWorld(str) {
 }
 
 console.log(countLastWorld(str));
+
+const s = "fjnfndf fewfhioj2901u43 fndsjkfnkfw      "
+const ss = s.replaceAll(/[0-9] | [^a-zA-Z]/g, " ").trim()
+let cnt = 0
+for (let i = ss.length - 1; i >= 0; i--) {
+    if (ss.charAt(i) === " ") {
+        console.log(cnt);
+        break;
+    }
+    cnt++;
+}
